fbd73d66adbbc494abd5e1e4c6d3da4fcb7bc5ce
Alejo <moyano887@gmail.com>
2023-04-01 20:20:40 -0300

    pagina terminada

--- src/products/productos.js
+++ src/products/productos.js
@@ -2,7 +2,7 @@ const products = [
     {
         id: 1,
         img300: "https://tse4.mm.bing.net/th?id=OIP.DAiScd8kFk-nOXEx7pd0JwAAAA&pid=Api&P=0" ,
-        img500: "https://i.ibb.co/nfKxnfv/chinchulin-500.jpg",
+        img500: "https://tse4.mm.bing.net/th?id=OIP.cbfBvZJQJNUyqlNCkBa1iQHaHa&pid=Api&P=0",
         title: "Skyy",
         description: "Vodka SKYY sabor Damasco, especial para tus fiestas",
         price: 2000,
@@ -12,7 +12,7 @@ const products = [
     {
         id: 2,
         img300: "https://cdn.shoplightspeed.com/shops/636686/files/21901460/fernet-branca-750-ml.jpg",
-        img500: "https://i.ibb.co/nfKxnfv/chinchulin-500.jpg",
+        img500: "https://tse2.mm.bing.net/th?id=OIP.vVEZAM1oR2eFmeYKva24awHaHa&pid=Api&P=0",
         title: "Fernet Branca",
         description: "Fernet grande, especial para juntarte con tus amigos",
         price: 1500,
@@ -22,7 +22,7 @@ const products = [
     {
         id: 3,
         img300: "http://wallpapercave.com/wp/KGFbRP3.jpg",
-        img500: "https://i.ibb.co/nfKxnfv/chinchulin-500.jpg",
+        img500: "https://1.bp.blogspot.com/-_q6Jwvf86uY/UQYh4NDqaWI/AAAAAAAAAjk/EEtMQpwWYik/s1600/MONSTER+ENERGY.jpg",
         title: "Monster",
         description: "Especial para acompañar tus bebidas",
         price: 600,
@@ -32,7 +32,7 @@ const products = [
     {
         id: 4,
         img300: "https://tse4.mm.bing.net/th?id=OIP.OlN9pbIZvMw_07jt2YWJfwHaLH&pid=Api&P=0",
-        img500: "https://i.ibb.co/nfKxnfv/chinchulin-500.jpg",
+        img500: "http://cdn2.salud180.com/sites/default/files/field/image/2017/10/coca_cola_vidrio_.jpg",
         title: "Coca Cola",
         description: "Coca Cola de vidrio, especial para tus fiestas",
         price: 1000,
